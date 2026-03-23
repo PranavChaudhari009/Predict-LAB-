404aaa69aabe219d50070fb19dab04cd4c2eb0d9
PRANAV <chaudharipranav175@gmail.com>
2026-03-23 22:28:45 +0530

    Add files via upload

---
+++ Predict LAB/app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="PredictLab", layout="wide")
+
+st.title("PredictLab – ML Prediction Platform")
+
+st.write("""
+Welcome to PredictLab.
+
+This platform demonstrates multiple machine learning workflows:
+- Classification
+- Regression
+- NLP
+- Clustering
+""")
+
+st.info("Use the sidebar to navigate between modules.")
