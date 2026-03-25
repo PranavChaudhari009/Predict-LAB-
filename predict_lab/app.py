8ddd255ac1eb12f2b599dca026ce447ce48fcb24
PRANAV <chaudharipranav175@gmail.com>
2026-03-25 12:07:53 +0530

    Update app.py

--- predict_lab/app.py
+++ predict_lab/app.py
@@ -9,25 +9,24 @@ Welcome to PredictLab.
 
 This platform demonstrates multiple machine learning workflows:
 """)
-st.header("Classification"),
-st.sudheader("Loan Approval Prediction")
+
+st.header("Classification")
+st.subheader("Loan Approval Prediction")
 
 st.header("Regression")
 st.subheader("House Price Prediction")
-         
+
 st.header("NLP")
 st.write("1. Email Spam Classifier")
 st.write("2. Comment Sentiment Analysis")
 st.write("3. Fake News Detection")
 
 st.header("Clustering")
-st.subheader("Customer Chrun")
 
 st.header("Time Series")
-st.subheader("Stock Price Movement ")
+st.subheader("Stock Price Movement")
 
-st.header("Recommedation system ")
-st.subheader("E-commerce Product Recommedations")
-
+st.header("Recommendation System")
+st.subheader("E-commerce Product Recommendations")
 
 st.info("Use the sidebar to navigate between modules.")
