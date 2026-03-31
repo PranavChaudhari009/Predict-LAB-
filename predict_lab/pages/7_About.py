e54885139fca6659d40c7ca64b295429ffc41d6b
PranavChaudhari009 <chaudharipranav175@gmail.com>
2026-03-31 16:06:14 +0530

    Create 7_About.py

---
+++ predict_lab/pages/7_About.py
@@ -0,0 +1,33 @@
+import streamlit as st 
+
+st.title("Project Overview")
+st.write("""PredictLab is a multi-module machine learning platform that allows users to explore classification, regression, NLP, clustering, time series, and recommendation systems 
+         through an interactive interface.""")
+
+st.title("Features")
+st.markdown("""
+- **Classification**: Predict loan approval based on applicant data.    
+- **Regression**: Estimate house prices using features like size, location, and amenities.
+- **NLP**: Analyze text data for spam detection, sentiment analysis, and fake news classification.
+- **Clustering**: Group customers based on purchasing behavior for targeted marketing.
+- **Time Series**: Forecast stock price movements using historical data and technical indicators.
+- **Recommendation System**: Provide personalized product recommendations based on user behavior and preferences.""")
+
+st.title("Technologies Used")
+st.markdown("""     
+- Python
+- Streamlit     
+- Scikit-learn
+- Pandas
+- NumPy
+- Matplotlib
+- Seaborn   
+- Natural Language Toolkit (NLTK)""")
+
+st.title("Future Enhancements")
+st.markdown("""
+- Integrate deep learning models for improved performance.  
+- Dataset upload feature
+- Real-time data integration for time series forecasting.
+- Enhanced visualization and interpretability tools.""") 
+            
